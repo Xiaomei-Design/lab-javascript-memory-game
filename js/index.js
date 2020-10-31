7805ba2dff1f35aca032ccb5ddb6044362149a3f
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-10-31 21:29:15 +0100

    memoryGame class

--- js/index.js
+++ js/index.js
@@ -42,8 +42,5 @@ window.addEventListener('load', event => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
+      
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,41 @@
+// let newGame = MemoryGame ();
+
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    if (this.cards === undefined) {
+      return undefined;
+    };
+    let newCards = [];
+    const length = this.cards.length;
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random()* this.cards.length);
+      newCards.push(this.cards[randomIndex]);
+      this.cards.splice(randomIndex, 1);
+    };
+    this.cards = newCards;
+  }
+
+  checkIfPair(card1, card2) {
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed < this.cards.length / 2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
